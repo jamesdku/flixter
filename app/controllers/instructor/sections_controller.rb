810679d977f79d56d713043e2f876da7558ef73f
James McJunkin <jmcj88@gmail.com>
2019-08-05 03:31:11 +0000

    New section form modal

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -3,31 +3,28 @@ class Instructor::SectionsController < ApplicationController
   before_action :require_authorized_for_current_course, only: [:create]
   before_action :require_authorized_for_current_section, only: [:update]
 
-  def new
-    @section = Section.new
-  end
-
   def create
     @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
   end
 
-    def update
+  def update
     current_section.update_attributes(section_params)
-    render plain: 'updated!'
+    render plain: 'ok!'
   end
 
   private
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
+      render plain: "Unauthorized", status: :unauthorized
     end
   end
 
   def current_section
     @current_section ||= Section.find(params[:id])
   end
+
 
   def require_authorized_for_current_course
     if current_course.user != current_user
@@ -37,11 +34,7 @@ class Instructor::SectionsController < ApplicationController
 
   helper_method :current_course
   def current_course
-    if params[:course_id]
-      @current_course ||= Course.find(params[:course_id])
-    else
-      current_section.course
-    end
+    @current_course ||= Course.find(params[:course_id])
   end
 
   def section_params
